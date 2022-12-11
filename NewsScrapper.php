99e286f9a52ec9b1907de3379f95dac2ffc6f95a
Takudzwa A Vafana <vafanat@gmail.com>
2022-12-11 15:42:43 +0200

    comments for logic flow

--- NewsScrapper.php
+++ NewsScrapper.php
@@ -2,6 +2,7 @@
 
 require 'vendor/autoload.php';
 
+
 use Goutte\Client;
 
 function scrapePage($url, $client, $file)
@@ -14,7 +15,17 @@ function scrapePage($url, $client, $file)
         $imageUrl = $node->filter('img')->attr('src');
         $comments = $node->filter('.symbol-text > span')->text();
 
+        // store data in a received array
+
         fputcsv($file, [$title, $dateCreated, $imageUrl,  $comments ]);
+
+        // SQL command to select title and id   SELECT `id`, `title` FROM `news` WHERE 1
+        // check if the result if not null, if null skip to adding to database, if not null store values in an current extisiting array
+        // use for loop to see if the data incoming exists in our current existing array if not intsert the value to the db table
+        // SQL command to insert  INSERT INTO `news`(`id`, `title`, `image`, `date_added`, `comment_count`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]')
+        // if it is existing check the update the date_added and the comment count
+        // SQL update Database UPDATE `news` SET `date_added`='[value-4]',`comment_count`='[value-5]' WHERE `id`= id
+
     });
 
     try {
