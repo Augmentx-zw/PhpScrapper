9abdcea3ea2ff172fbc2a1e7193c236edcf0d260
Takudzwa A Vafana <vafanat@gmail.com>
2022-12-12 12:16:46 +0200

    screpper push

--- NewsScrapper.php
+++ NewsScrapper.php
@@ -1,42 +1,38 @@
 <?php
 
+
+require 'vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+use Goutte\Client;
+
 $servername = "127.0.0.1";
 $username = "root";
 $password = "";
 $dbname = "news";
 
 
-
-require 'vendor/autoload.php';
-
-
-use Goutte\Client;
-
-function scrapePage($url, $client, $file, $conn)
+function scrapePage($url, $client, $conn)
 {
 
     $sql = "SELECT 'title' FROM `news`";
-    $dataRes = array();
-    $Existingresult = $conn->mysql_query($sql);
+    $Existingresult = (array) $conn->query($sql);
 
-    print($Existingresult);
-    
     $crawler = $client->request('GET', $url);
 
-    $crawler->filter('.card-item')->each(function ($node) use ($conn, $dataRes) {
+    $crawler->filter('.card-item')->each(function ($node) use ($conn, $Existingresult) {
         $title = $node->filter('.card-item__title')->text();
         $dateCreated = $node->filter('.symbol-text > time')->text();
         $imageUrl = $node->filter('img')->attr('src');
         $comments = $node->filter('.symbol-text > span')->text();
 
-        if (in_array($title, $dataRes, TRUE)) {
+        if (in_array($title, $Existingresult, TRUE)) {
             $updatesql = "UPDATE `news` SET `date_added`='$dateCreated' ,`comment_count`= '$comments' WHERE `title`= '$title'";
               $conn->query($updatesql);
         } else {
              $instertSql = "INSERT INTO `news`(`id`,`title`, `image`, `date_added`, `comment_count`) VALUES (NULL,'$title', '$imageUrl', '$dateCreated' ,'$comments')";
               $conn->query($instertSql);
         }
-
     });
 
     try {
@@ -49,7 +45,6 @@ function scrapePage($url, $client, $file, $conn)
 }
 
 $client = new Client();
-$file = fopen("news.csv", "w");
 $url = "https://www.gamespot.com/news/";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -57,8 +52,7 @@ if ($conn->connect_error) {
 }
 
 while ($url) {
-    echo "<h2>" . $url . "</h2>" . PHP_EOL;
-    $url = scrapePage($url, $client, $file, $conn);
+    echo $url.PHP_EOL;
+    $url = scrapePage($url, $client, $conn);
 }
 $conn->close();
-fclose($file);
